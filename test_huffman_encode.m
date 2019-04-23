c368d0434da7aba1f78d33a5e469da16a7d253a5
Chris Hemingway <chris.hemingway@gmail.com>
2019-04-23 11:21:00 +0100

    Compare huffman encoding Cr to zip.

--- test_huffman_encode.m
+++ test_huffman_encode.m
@@ -4,6 +4,8 @@
 addpath('./tinevez-matlab-tree-3d13d15');
 
 % Arbitrary message encode, shows performance results
+text_file = 'sample_text.txt';
+
 message = fileread('sample_text.txt');
 pmap = probability_map(message);
 
@@ -20,9 +22,20 @@ Cr_max = 8/h; % Max possible Cr
 
 fprintf("Bits before = %d, after = %d \n",n1,n2);
 fprintf("Message Entropy = %.3f \n",h);
-fprintf("Achieved Cr = %.3f, %02.2f%% of Theoretical Cr = %.3f \n",... 
+fprintf("Huffman achieved Cr = %.3f, %02.2f%% of Theoretical Cr = %.3f \n",... 
         Cr, (Cr/Cr_max)*100, Cr_max);
 
+% Compare against zip
+zipfile_name = [text_file '.zip'];
+zip(zipfile_name,text_file);
+zipped_stats = dir(zipfile_name);
+zipped_bits = zipped_stats.bytes * 8;
+
+fprintf("Zipped file is %d bits, Cr = %.3f \n",...
+    zipped_bits,n1/zipped_bits); 
+
+delete(zipfile_name); % Clean up
+    
 function h = pmap_entropy(pmap)
     pk = cell2mat(values(pmap));
     h = - sum(pk .* log2(pk));
